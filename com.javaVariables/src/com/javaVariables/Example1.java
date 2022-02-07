1fa68fd803986f68b19f275ef3aae53ea6e4c713
nikitha <nikitha@LAPTOP-4GAKUODQ>
2022-02-07 11:04:26 +0530

    first commit

---
+++ com.javaVariables/src/com/javaVariables/Example1.java
@@ -0,0 +1,37 @@
+package com.javaVariables;
+
+public class Example1 {
+
+	public static void main(String[] args) 
+	{
+		// Write a java program to print student information in java variables
+		
+		//variable declaration
+		int studentId;
+		String studentName;
+		float studentMarks;
+		String studentAddress;
+		
+		//variable initilization
+		
+		studentId = 3020;
+		studentName = "Nikitha";
+		studentMarks = 50.78f;
+		studentAddress = "India";
+		
+		System.out.print("Student Id: ");
+		System.out.println(studentId);
+		
+		System.out.print("Student Name: ");
+		System.out.println(studentName);
+		
+		System.out.print("Student Marks: ");
+		System.out.println(studentMarks);
+		
+		System.out.print("Student Address: ");
+		System.out.println(studentAddress);
+		
+
+	}
+
+}
